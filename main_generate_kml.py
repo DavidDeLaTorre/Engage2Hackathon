66a953edf1b71e098bb66d3bc8a11e42ea222065
David de la Torre <david.de.la.torre.sangra@upc.edu>
2025-03-24 14:27:54 +0100

    Add kml script.

---
+++ main_generate_kml.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Main script to generate a KML with all flights for one entire day.
+This script imports functions from tools_load_many.py and tools_misc.py.
+Usage:
+    python main_generate_kml.py <year> <month> <day> <output_kml_file>
+Example:
+    python main_generate_kml.py 2024 11 16 output.kml
+"""
+
+import sys
+
+from tools_filter import filter_dataframe_by_bounds, filter_dataframe_by_altitude
+from tools_import import load_parquet_files
+from tools_export import export_trajectories_to_kml
+
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: python main_generate_kml.py <year> <month> <day> <output_kml_file>")
+        sys.exit(1)
+
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
+    day = int(sys.argv[3])
+    output_kml = sys.argv[4]
+
+    # For one entire day, process all hours from 0 to 23
+    df = load_parquet_files(
+        year, month, day, 0,
+        year, month, day, 23,
+        base_path="data/engage-hackathon-2025")
+
+    if df.empty:
+        print("No data found for the specified day.")
+        sys.exit(1)
+
+    # Filter dataframe by geographical coordinates around Madrid
+    min_lat, max_lat, min_lon, max_lon = [40.0, 41.0, -4.0, -3.0]  # [deg]
+    df = filter_dataframe_by_bounds(df, min_lat, max_lat, min_lon, max_lon)
+    min_alt, max_alt = [0, 1000]  # [ft]
+    df = filter_dataframe_by_altitude(df, min_alt, max_alt)
+
+    # Generate the KML with all flights
+    export_trajectories_to_kml(df, output_kml)
+
+
+if __name__ == '__main__':
+    main()
